4b8142985140459afd58a989af47cf7ea1193561
19020200 <19020200@DESKTOP-3FUCLFB>
2020-08-20 13:55:59 +0800

    Further update on C206_CaseStudy main and outlet classes

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -10,9 +10,17 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Staff> staffList = new ArrayList<Staff>();
+		ArrayList<Customer> customerList = new ArrayList<Customer>();
+		ArrayList<Outlet> outletList = new ArrayList<Outlet>();
+		ArrayList<Transaction> transactionList = new ArrayList<Transaction>();
+		
+		
 		staffList.add(new Staff(12, "Tom", "12122000"));
 		staffList.add(new Staff(13, "Tim", "09112000"));
-		
+		customerList.add(new Customer(1, "Lisa", "88294751"));
+		customerList.add(new Customer(2, "Chad", "95667208"));
+		outletList.add(new Outlet(898,"Singapore Outlet","Karen","West Coast Outlet","Jurong Outlet"));
+		outletList.add(new Outlet(898,"Singapore Outlet","Karen","West Coast Outlet","Jurong Outlet"));
 		
 
 		int option = -1;
@@ -20,10 +28,11 @@ public class C206_CaseStudy {
 		while (option != 6) {
 
 			System.out.println("1. Staff");
-			System.out.println("2. -");
-			System.out.println("3. -");
-			System.out.println("4. -");
-			System.out.println("5. Quit");
+			System.out.println("2. Product");
+			System.out.println("3. Customer");
+			System.out.println("4. Outlet");
+			System.out.println("5. Transaction");
+			System.out.println("6. Quit");
 			Helper.line(80, "-");
 			option = Helper.readInt("Enter an option : ");
 
@@ -47,6 +56,22 @@ public class C206_CaseStudy {
 			} else if (option == 3) {
 				
 			} else if (option == 4) {
+				 option = Helper.readInt("Enter an option > ");
+				    
+			      if (option ==1) {
+			        //view all outlet
+			    	C206_CaseStudy.viewAllOutlet(outletList);    
+			      
+			      } else if (option ==2) {
+			        //add outlet
+			    	C206_CaseStudy.addAllOutlet(outletList);   
+			      
+			      } else if (option ==3) {
+			        //delete outlet
+			        C206_CaseStudy.delAllOutlet(outletList);
+			    } else {
+			        System.out.println("Invalid option");
+			    }
 	
 						
 			}else if (option == 5) {
@@ -59,6 +84,9 @@ public class C206_CaseStudy {
 
 		
 	}
+	
+	//================STAFF (ALVIN) =======================
+	
 //================View staff=======================
 	public static String retrieveAllStaff(ArrayList<Staff> staffList) {
 		String output="";
@@ -77,6 +105,7 @@ public class C206_CaseStudy {
 		 output += retrieveAllStaff(staffList);	
 		System.out.println(output);
 	}
+	
 //================Add staff=======================
 
 	public static void addStaff(ArrayList<Staff> staffList) {
@@ -87,6 +116,7 @@ public class C206_CaseStudy {
 		staffList.add(st);
 		System.out.println("Staff added");
 	}
+	
 //================Delete staff=======================  
 	public static void deleteStaff(ArrayList<Staff> staffList) {
 		if(staffList.size()==0) {
@@ -100,23 +130,27 @@ public class C206_CaseStudy {
 				staffpos=i;
 			}
 		}
-<<<<<<< HEAD
+
 		String yesNno=Helper.readString("You confirm want to delete ? (Yes/No):");
 		if (yesNno.equalsIgnoreCase("Yes")) {
-=======
+
 		String yes_no=Helper.readString("You confirm want to delete ? (Yes/No):");
 		Object option;
 		if (yes_no.equalsIgnoreCase("Yes")) {
->>>>>>> branch 'master' of https://github.com/19020200-Bunga-Thalia/C206_CaseStudy
+
 			staffList.remove(staffpos);
 			System.out.println("Staff has been deleted");
-// =====================PRODUCT===========================
+			
+			
+// =====================PRODUCT (KARLA)===========================
 
 ArrayList<Product> productList;
+
 			// ====================== view product =========================
 			for (int i = 0; i < productList.size(); i++) {
 				Product p = productList.get(i);
 				System.out.println(String.format("%-10s %-30s %-10d", getProductName,getCategory, getPrice );
+				
 // ======================= ADD PRODUCT========================= 
 				ArrayList<String> productList= new ArrayList<String>();
 				String productName = Helper.readString("Enter a product to add > ");
@@ -135,18 +169,15 @@ ArrayList<Product> productList;
 				    }
 		}
 		}
-<<<<<<< HEAD
 		}
-	public static void addStaff(ArrayList<Staff> staffList, Staff worker1) {
-		// TODO Auto-generated method stub
-		
-=======
->>>>>>> branch 'master' of https://github.com/19020200-Bunga-Thalia/C206_CaseStudy
 	}
-	public static void viewTransaction(Object productList) {
-		// TODO Auto-generated method stub
-		
-	}
+	
+	// =====================CUSTOMER(BUNGA)===========================
+	// =====================OUTLETS(GRACE)===========================
+	// =====================TRANSACTION(VALERIE)===========================
 }
+
+
+	
 
 
--- C206_CaseStudy/src/OutletDB.java
+++ C206_CaseStudy/src/OutletDB.java
@@ -1,4 +1,51 @@
+import java.util.ArrayList;
+  public class OutletDB {
+    //sub class
+    //GRACE
 
-public class OutletDB {
+    public static ArrayList<Outlet> outletList = new ArrayList<>();
+
+
+    public static void addOutlet(Outlet outlet) {
+
+      outletList.add(outlet);
+      
+    }
+    public static String viewAllOutlet() {
+      String output = "";
+      int check = 0;
+      for(int i =0; i < outletList.size();i++) {
+        output +="Outlet Name: " + outletList.get(i).getOutlet_name() + "\n";
+        output +="Outlet ID: " + outletList.get(i).getOutlet_id() +"\n";
+        output +="Staff Outlet: " + outletList.get(i).getStaff_outlet() +"\n";
+        check = 1;
+      }
+      if(check == 0) {
+        output ="No Outlet information existed ";
+      }
+      return output;
+    }
+    public static String delOutlet(String outlet_name){
+      String output = "Fail to delete";
+      for(int i =0; i < outletList.size();i++) {
+        if(outletList.get(i).getOutlet_name().equalsIgnoreCase(outlet_name)) {
+          outletList.remove(i);
+          output = "Delete succesful";
+          break;
+        }  
+      }
+      return output;
+      }
+    
+      
+    public static void showOutletMenu() {
+      System.out.println("1.Add outlet");
+      System.out.println("2.View outlet");
+      System.out.println("3.Delete outlet");
+      System.out.println("4.Quit");
+    
+  }
+
+
 
 }
