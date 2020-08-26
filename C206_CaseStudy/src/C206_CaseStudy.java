694c02f8bd16bf383223e61fc0d7f5589bb1a3c1
19001350 <19001350@DESKTOP-S2PE81P>
2020-08-26 18:28:55 +0800

    Update Staff and Link to outlet

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -22,8 +22,8 @@ public class C206_CaseStudy {
 		ArrayList<Transaction> transactionList = new ArrayList<Transaction>();
 		ArrayList<Transaction> archiveList = new ArrayList<Transaction>();
 
-		staffList.add(new Staff(12, "Tom", "12122000"));
-		staffList.add(new Staff(13, "Tim", "09112000"));
+		staffList.add(new Staff(12, "Tom", "12122000", 898));
+		staffList.add(new Staff(13, "Tim", "09112000", 898));
 		customerList.add(new Customer(1, "Lisa", 88294751, 1));
 		customerList.add(new Customer(2, "Chad", 95667208, 2));
 		outletList.add(new Outlet(898, "Singapore Outlet", "Karen", "West Coast Outlet", "Jurong Outlet"));
@@ -46,6 +46,7 @@ public class C206_CaseStudy {
 				System.out.println("1. Add");
 				System.out.println("2. Delete");
 				System.out.println("3. View All");
+				System.out.println("4. Update");
 				int suboption = Helper.readInt("Enter option :");
 				if (suboption == 1) {
 					C206_CaseStudy.addStaff(staffList);
@@ -53,7 +54,11 @@ public class C206_CaseStudy {
 					C206_CaseStudy.deleteStaff(staffList);
 				} else if (suboption == 3) {
 					C206_CaseStudy.viewAllStaff(staffList);
-				} else {
+				} else if (suboption == 4) {
+					C206_CaseStudy.updateStaff(staffList);
+				}
+				
+				else {
 					System.out.println("Invalid option");
 				}
 			} else if (option == 2) {
@@ -151,8 +156,8 @@ public class C206_CaseStudy {
 	public static String retrieveAllStaff(ArrayList<Staff> staffList) {
 		String output = "";
 		for (int i = 0; i < staffList.size(); i++) {
-			output += String.format("%-25d %-25s %-25s \n", staffList.get(i).getStaff_id(),
-					staffList.get(i).getStaff_name(), staffList.get(i).getStaff_birthdate());
+			output += String.format("%-25d %-25s %-25s %-25s \n", staffList.get(i).getStaff_id(),
+					staffList.get(i).getStaff_name(), staffList.get(i).getStaff_birthdate(), staffList.get(i).getStaff_outletID());
 		}
 		return output;
 	}
@@ -161,7 +166,7 @@ public class C206_CaseStudy {
 		Helper.line(80, "-");
 		System.out.println("STAFF LIST");
 		Helper.line(80, "-");
-		String output = String.format("%-25s %-25s %-25s \n", "STAFF ID", "STAFF NAME", "BIRTHDATE");
+		String output = String.format("%-25s %-25s %-25s %-25s \n", "STAFF ID", "STAFF NAME", "BIRTHDATE", "OUTLET ID");
 		output += retrieveAllStaff(staffList);
 		System.out.println(output);
 	}
@@ -172,7 +177,8 @@ public class C206_CaseStudy {
 		int staff_id = Helper.readInt("Enter Staff ID : ");
 		String staff_name = Helper.readString("Enter Staff name : ");
 		String staff_birthdate = Helper.readString("Enter birthdate (DDMMYYYY) : ");
-		Staff st = new Staff(staff_id, staff_name, staff_birthdate);
+		int staff_outletID = Helper.readInt("Enter staff outlet ID: ");
+		Staff st = new Staff(staff_id, staff_name, staff_birthdate,staff_outletID);
 		staffList.add(st);
 		System.out.println("Staff added");
 	}
@@ -205,6 +211,25 @@ public class C206_CaseStudy {
 			}
 		}
 	}
+	//================Update staff=======================  
+	public static void updateStaff(ArrayList<Staff> staffList) {
+		int updatestaff1 = Helper.readInt("Enter ID of Staff to update: ");
+		for (int i = 0; i < staffList.size(); i++) {
+			if (updatestaff1 == staffList.get(i).getStaff_id()) {
+				int update_staffid = Helper.readInt("Enter new staff ID: ");
+				String update_staffname = Helper.readString("Enter new name ");
+				String update_staffbirthdate = Helper.readString("Enter new birthdate: ");
+				int update_staffoutlet = Helper.readInt("Enter new outlet ID: ");
+				staffList.get(i).setStaff_id(update_staffid);
+				staffList.get(i).setStaff_name(update_staffname);
+				staffList.get(i).setStaff_birthdate(update_staffbirthdate);
+				staffList.get(i).setStaff_outletID(update_staffoutlet);
+				
+
+	}
+		}
+	}
+	
 	// =====================PRODUCT (KARLA)===========================
 
 	// ====================== view product =========================
--- C206_CaseStudy/src/Staff.java
+++ C206_CaseStudy/src/Staff.java
@@ -3,11 +3,13 @@ public class Staff {
 private int staff_id;
 private String staff_name;
 private String staff_birthdate;
+private int staff_outletID;
 
-public Staff(int staff_id, String staff_name, String staff_birthdate) {
+public Staff(int staff_id, String staff_name, String staff_birthdate, int staff_outletID) {
 	this.staff_id = staff_id;
 	this.staff_name = staff_name;
 	this.staff_birthdate = staff_birthdate;
+	this.staff_outletID = staff_outletID;
 }
 
 public int getStaff_id() {
@@ -33,6 +35,14 @@ public String getStaff_birthdate() {
 public void setStaff_birthdate(String staff_birthdate) {
 	this.staff_birthdate = staff_birthdate;
 }
-
+public int getStaff_outletID() {
+	return staff_outletID;
+}
+public void setStaff_outletID(int staff_outletID) {
+	this.staff_outletID=staff_outletID;
+}
 
 }
+
+
+
